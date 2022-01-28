4f62f01a23071cd3701a492aa99c6835294beb68
alvkeke <alvkeke@126.com>
2022-01-29 01:44:49 +0800

    [update] add some comment before function declaration

--- algorithms/base64.h
+++ algorithms/base64.h
@@ -6,7 +6,24 @@ License: MIT
 #ifndef __ALGO_BASE64_H__
 #define __ALGO_BASE64_H__
 
+/**
+ * @brief encode data to base64, `free` need to be called for releasing result string.
+ * 
+ * @param src source data
+ * @param n_src length of source data
+ * @param n_ret length of result string
+ * @return result string, NULL for failed.
+ */
 char *algo_base64_encode(const char *src, int n_src, int *n_ret);
+
+/**
+ * @brief decode an encoded-base64-string, `free` need to be called for release result string.
+ * 
+ * @param src encoded string
+ * @param n_src length of the encoded string.
+ * @param n_ret length of the original data.
+ * @return original data
+ */
 char *algo_base64_decode(const char *src, int n_src, int *n_ret);
 
 #endif // __ALGO_BASE64_H__
